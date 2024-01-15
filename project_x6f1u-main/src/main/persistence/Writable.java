d02aaaa3842427300be4778a9da14b0f65982130
Violet Yang <115138850+VYang4@users.noreply.github.com>
2024-01-15 00:48:34 -0800

    Delete project_x6f1u-main directory

--- project_x6f1u-main/src/main/persistence/Writable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-// Adapted from JsonSerializationDemo
-public interface Writable {
-    // EFFECTS: returns this as JSON object
-    JSONObject toJson();
-}
